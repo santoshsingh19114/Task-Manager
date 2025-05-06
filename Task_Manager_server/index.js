18dd4efb88448993210c070b25e469e22e64e5ce
Santosh kumar <118276537+santoshsingh19114@users.noreply.github.com>
2025-05-06 15:18:24 +0530

    Update index.js

--- Task_Manager_server/index.js
+++ Task_Manager_server/index.js
@@ -22,7 +22,7 @@ const PORT= process.env.PORT || 5000
 const app=express()
 
 app.use(cors({
-    origin: ["http://localhost:3000","http://localhost:3001"],
+    origin: ["http://localhost:3000","http://localhost:3001","https://task-manager-pyrate.netlify.app/"],
     methods:["GET","PUT","POST","DELETE"],
     credentials: true,
 }));
