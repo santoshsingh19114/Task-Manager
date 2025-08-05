548bcba6e614a9c23193dfa530d2286db9aa4e32
santosh <sk1155610@gmail.com>
2025-08-06 01:44:46 +0530

    minor error

--- Task_Manager_server/utils/index.js
+++ Task_Manager_server/utils/index.js
@@ -42,7 +42,7 @@ export const createJWT = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
